1d6a40aa3a1f816f433f48794661c4e520f63386
Baptiste Régnier <baptiste.regnier62@gmail.com>
2023-07-04 15:04:17 +0200

    division en deux du tableau

--- algo.py
+++ algo.py
@@ -1,12 +1,20 @@
 from random import randint
 
 tab_min = 0
-tab_max = 100
+tab_max = 99
 tab_length = randint(tab_min, tab_max)
+number = randint(tab_min, tab_max)
 tab = []
 
 for k in range(tab_length):
     tab.append(randint(tab_min, tab_max))
+    tab.sort()
 
-tab.sort()
-print(tab)
+tab_left = []
+tab_right = tab
+
+for i in range(len(tab)//2):
+    tab_left.append(tab[i])
+    tab_right.pop(i)
+
+print(tab_left, tab_right)
